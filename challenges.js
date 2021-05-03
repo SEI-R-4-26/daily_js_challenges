b1fd30ba734c3d197dda7ec75143987ea1a38751
Emily Westerback <emilywesterback2@gmail.com>
2021-05-03 07:46:34 -0700

    second challenge complete

--- challenges.js
+++ challenges.js
@@ -31,7 +31,9 @@ addOne(1) //=> 2
 addOne(-5) //=> -4
 -----------------------------------------------------------------*/
 // Your solution for 01-addOne here:
-
+function addOne(num) {
+  return num + 1
+}
 /*-----------------------------------------------------------------
 Challenge: 02-addTwoNumbers
 
@@ -50,7 +52,12 @@ addTwoNumbers(0, 0) //=> 0
 addTwoNumbers('Hello', 5) //=> NaN
 -----------------------------------------------------------------*/
 // Your solution for 02-addTwoNumbers here:
-
+function addTwoNumbers(x, y) {
+  if (isNaN(x) || isNaN(y)) {
+    return NaN
+  }
+  return x + y
+}
 /*-----------------------------------------------------------------
 Challenge: 03-sumNumbers
 
