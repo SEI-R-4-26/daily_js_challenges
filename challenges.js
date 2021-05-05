787f40cc47115a2fafc19099aaea56cfdc85039f
Lindsey Moss <llmodbls@gmail.com>
2021-05-05 17:55:36 -0400

    through 05 complete

--- challenges.js
+++ challenges.js
@@ -85,7 +85,13 @@ sumNumbers([]) //=> 0
 -----------------------------------------------------------------*/
 // Your solution for 03-sumNumbers here:
 
-
+const sumNumbers = (arr) => {
+  let sum = 0;
+  for (i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
 
 /*-----------------------------------------------------------------
 Challenge: 04-addList
@@ -106,28 +112,42 @@ add(7,-12) //=> -5
 -----------------------------------------------------------------*/
 // Your solution for 04-addList here:
 
+function addList() {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i]
+  }
+  return sum
+}
 
 
 /*-----------------------------------------------------------------
 Challenge: 05-computeRemainder
-
+ 
 Difficulty: Basic
-
+ 
 Prompt:
-
+ 
 - Write a function named computeRemainder that accepts two numeric arguments and returns the remainder of the division of those two numbers.
 - The first argument should be the dividend and the second argument should be the divisor.
 - If a 0 is passed in as the second argument you should return JavaScript's special numeric value: Infinity.
 - For extra fun, complete this challenge without using the modulus (%) operator.
-
+ 
 Examples:
-
+ 
 computeRemainder(10,2) //=> 0
 computeRemainder(4,0) //=> Infinity
 computeRemainder(10.5, 3) //=> 1.5
 -----------------------------------------------------------------*/
 // Your solution for 05-computeRemainder:
 
+const computeRemainder = (num1, num2) => {
+  if (num2 === 0) {
+    return Infinity
+  } else {
+    return num1 % num2
+  }
+}
 
 
 /*-----------------------------------------------------------------
